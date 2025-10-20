467bc83ea01566d22728a537f10acdfdcd70e106
Indigo-Hawk <indigo.ehawk@gmail.com>
2025-10-20 18:30:54 -0400

    added reset

--- Globals.gd
+++ Globals.gd
@@ -6,6 +6,10 @@ var PlayerPos:Vector2 = Vector2.ZERO
 
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("RESET"):
+		get_tree().reload_current_scene()
+
 func changeScenes(FileName:String) ->void:
 	get_tree().call_deferred("change_scene_to_file",FileName)
 	call_deferred("_resetPos")
