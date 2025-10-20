c2afafb3e6ff8e9ffa26505a19671f9806a8e480
Indigo-Hawk <indigo.ehawk@gmail.com>
2025-10-20 19:03:19 -0400

    added stuff to main menu

--- Components/Head/head.gd
+++ Components/Head/head.gd
@@ -9,9 +9,21 @@ func _physics_process(delta: float) -> void:
 	if !isFree:
 		return
 	
-	var x = -Input.get_action_strength("LEFT") + Input.get_action_strength("RIGHT");
+	var x = -Input.get_action_strength("LEFT") + Input.get_action_strength("RIGHT")
 	if x != 0:
 		linear_velocity.x += x * SPEED * delta
 			
 	Globals.PlayerPos = global_position
+	
+	if abs(linear_velocity.length()) < 1:
 		
+		if $Timer.get_time_left() == 0:
+			
+			$Timer.start()
+	if abs(linear_velocity.length()) > 1:
+		$Timer.stop()
+		
+
+
+func _on_timer_timeout() -> void:
+	$Label.visible = true 
--- Components/UI/main_menu.gd
+++ Components/UI/main_menu.gd
@@ -2,8 +2,16 @@ extends Control
 
 
 func _on_button_button_up() -> void:
-			get_tree().call_deferred("change_scene_to_file","res://test.tscn")
+			get_tree().call_deferred("change_scene_to_file","res://Levels/test.tscn")
 
 
 func _on_button_2_button_up() -> void:
 	get_tree().quit(0)
+
+
+func _on_credits_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_up() -> void:
+	pass # Replace with function body.
