71c62a6533d507138615996182704986d6115858
Indigo-Hawk <indigo.ehawk@gmail.com>
2025-10-22 16:17:14 -0400

    Changed how head physics updates (prevents race conditions) added functional portals!

--- Components/Head/head.gd
+++ Components/Head/head.gd
@@ -4,30 +4,45 @@ class_name Head
 @export var SPEED = 100.0
 
 var isFree = true
+var lastDelta:float= 0
+var teleporting:bool = false
+var teleportingPos:Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
+	lastDelta = delta
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	
+	if teleporting:
+		global_position = teleportingPos 
+		teleporting = false   
 	
 	$Control.rotation = -rotation
 	
 	if !isFree:
 		return
 	
+	if Input.is_action_just_pressed("DOWN"):
+		teleportTo(Vector2.ZERO)
+	
 	var x = -Input.get_action_strength("LEFT") + Input.get_action_strength("RIGHT")
 	if x != 0:
-		linear_velocity.x += x * SPEED * delta
+		linear_velocity.x += x * SPEED * lastDelta
 			
 	Globals.PlayerPos = global_position
 	
 	if abs(linear_velocity.length()) < 1:
-		
 		if $Timer.get_time_left() == 0:
-			
 			$Timer.start()
+			
 	if abs(linear_velocity.length()) > 1:
 		$Timer.stop()
 		$Control/Label.visible = false
 		
-
+func teleportTo(POS:Vector2) -> void:
+	teleporting = true
+	teleportingPos = POS
+	#linear_velocity *= 0
 
 func _on_timer_timeout() -> void:
 	$Control/Label.visible = true 
--- Components/Portal/portal.gd
+++ Components/Portal/portal.gd
@@ -1,7 +1,31 @@
 extends Area2D
+class_name Portal
+
+
+@export var connectedPortal:Portal
+var justTeleported:bool = false
+
+func _ready() -> void:
+	if connectedPortal == null:
+		push_error("Portal has no connected Portal")
 
 
 
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body is Head:
+		if justTeleported:
+			return
+			
+		connectedPortal.justTeleported = true
+		justTeleported = true
+		
+		if connectedPortal != null:
+			body.teleportTo(connectedPortal.global_position)
+		$Timer.start()
+			
+
+
+func _on_timer_timeout() -> void:
+	justTeleported = false
+	connectedPortal.justTeleported = false
